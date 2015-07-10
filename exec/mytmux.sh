c9b7f4ab0f3571497ac9f2f7b4113addb65f007e
Aaron <aaron.chang@vpon.com>
2015-07-10 17:44:20 +0800

    Updaet tmux script

--- exec/mytmux.sh
+++ exec/mytmux.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 tmux start-server
 #tmux new-session -d -s work -n MC
@@ -10,16 +10,18 @@ tmux start-server
 #tmux new-window -t work:6 -n qemu-1.0 'cd ~/src/qemu/qemu-src/qemu-1.0/; bash -i'
 #tmux new-window -t work:7 -n Patrick 'cd ~/src/qemu/qemu-src/qemu-merge-android/qemu; bash -i'
 
-tmux new-session -d -s work -n OPS
-tmux new-window -t work:2 -n OPS2
-tmux new-window -t work:3 -n note
-tmux new-window -t work:4 -n v2bsp
-tmux new-window -t work:5 -n tmp
-tmux new-window -t work:6 -n gst
-tmux new-window -t work:7 -n CL_Android
-tmux new-window -t work:8 -n test
-tmux new-window -t work:9 -n emacs
-tmux new-window -t work:10 -n build
+# run rails server in port 3000 @ vpon-dsp
+tmux new-session -d -s work -n Rails 'cd ~/Documents/workspace/vpon-dsp; rails s'
 
-tmux select-window -t work:1
+# run babel @ vpon-dsp
+tmux new-window -t work:2 -n Babel 'cd ~/Documents/workspace/vpon-dsp; rm -rf public/app/flux/build/; babel public/app/flux/src/ -d public/app/flux/build --modules amd -w'
+
+# display mytop & iostat of server 156
+tmux new-window -t work:3 -n 56 'mytop -s 1 --noi --noheader -h 192.168.101.156 -u admin -p s123456'
+tmux select-window -t work:3
+tmux selectp -t 0
+tmux split -v -p 50 'ssh aaron.chang@192.168.101.156 -t "iostat -x 1"'
+
+# Show server 156 as default window
+tmux select-window -t work:3
 tmux attach-session -t work
