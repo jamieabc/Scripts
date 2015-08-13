e2c6cd6d0db91b9ee16c934be6b59eddfb5ed52b
Aaron <aaron.chang@vpon.com>
2015-08-13 17:44:58 +0800

    Update script file

--- exec/gen_tags.sh
+++ exec/gen_tags.sh
@@ -51,7 +51,7 @@ if [ "$1" == "e" ]; then
     #gtags -f gtags.files
     if [ ! -f GTAGS ]; then
         #gtags --gtagsconf ~/.emacs.d/gtags.conf
-        find . -type f -name "*.js" -o -name "*.jsx" -o -name "*.html" -o -name "*.json" -o -name "*.rb"| grep -vE '(node_modules|buld|.min.js$|__site_|.git)' > gtags.files
+        find . -type f -name "*.js" -o -name "*.jsx" -o -name "*.html" -o -name "*.json" -o -name "*.rb" -o -name "*.c" -o -name "*.h" -o -name "*.cpp" | grep -vE '(node_modules|buld|.min.js$|__site_|.git)' > gtags.files
         gtags -f gtags.files
     else
         #gtags -i --gtagsconf ~/.emacs.d/gtags.conf
--- exec/mytmux.sh
+++ exec/mytmux.sh
@@ -6,17 +6,20 @@ tmux start-server
 #tmux new-window -t work:2 -n OPS2
 #tmux new-window -t work:3 -n note
 
-# run rails server in port 3000 @ vpon-dsp
-tmux new-session -d -s work -n Rails 'cd ~/Documents/workspace/vpon-dsp; rails s'
+# start zeus in port 3000 @ vpon-dsp
+tmux new-session -d -s work -n Zeus 'cd ~/Documents/workspace/vpon-dsp; zeus start'
+
+# start rails server in zeus on port 3000 @ vpon-dsp
+tmux new-window -t work:1 -n Rails 'cd ~/Documents/workspace/vpon-dsp; sleep 5; zeus server'
 
 # run babel @ vpon-dsp
-tmux new-window -t work:1 -n Babel 'cd ~/Documents/workspace/vpon-dsp; rm -rf public/app/flux/build/; babel public/app/flux/src/ -d public/app/flux/build --modules amd -w'
+tmux new-window -t work:2 -n Babel 'cd ~/Documents/workspace/vpon-dsp; rm -rf public/app/flux/build/; babel public/app/flux/src/ -d public/app/flux/build --modules amd -w'
 
 # display mytop & iostat of server 156
-tmux new-window -t work:2 -n 56 'mytop -s 1 --noi --noheader -h 192.168.101.156 -u admin -p s123456'
-tmux select-window -t work:2
-tmux selectp -t 0
-tmux split -v -p 50 'ssh aaron.chang@192.168.101.156 -t "iostat -x 1"'
+#tmux new-window -t work:2 -n 56 'mytop -s 1 --noi --noheader -h 192.168.101.156 -u admin -p s123456'
+#tmux select-window -t work:2
+#tmux selectp -t 0
+#tmux split -v -p 50 'ssh aaron.chang@192.168.101.156 -t "iostat -x 1"'
 
 # Show server 156 as default window
 tmux select-window -t work:2
