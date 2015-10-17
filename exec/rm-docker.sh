8404e154d57a9e2acfe3a87f630f1d3d01a85666
Aaron <aaron.chang@vpon.com>
2015-10-17 20:54:30 +0800

    Modify docker remove script

--- exec/rm-docker.sh
+++ exec/rm-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-RM="curl -X DELETE 192.168.101.29:5000/v1/repositories/vpon/vpon-dsp-web-retargeting/tags/"
+RM="curl -X DELETE 192.168.101.29:5000/v1/repositories/vpon/vpon-dsp-web-dev-dsl/tags/"
 
-for i in {1..182}
+for i in {1..30}
 do
     String="$RM""$i"
     $($String)
