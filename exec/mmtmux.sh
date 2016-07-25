5ec4f82c776e0e4b669a8e6f80a8ab5c9c8e164a
Aaron Chang <aaron.chang@vpon.com>
2016-07-25 14:22:57 +0800

    update tmux file to copy MyGuardFile everytime guard starts

--- exec/mmtmux.sh
+++ exec/mmtmux.sh
@@ -26,6 +26,7 @@ then
     # guard live reload
     tmux new-window -t ${SESSION_NAME}:3 -n guard
     tmux send-keys -t ${SESSION_NAME}:3 'cd ~/Documents/Project/vpon-dsp/' C-m
+    tmux send-keys -t ${SESSION_NAME}:3 'cp ~/Documents/Env-Settings/MyGuardFile .' C-m
     tmux send-keys -t ${SESSION_NAME}:3 'guard -G MyGuardFile' C-m
 
     # tig
