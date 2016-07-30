5021e217fab641fa9948ce2d19ee876b51fc367b
Aaron Chang <aaron.chang@vpon.com>
2016-07-30 20:11:59 +0800

    add missing * when generating ctags file list

--- exec/gen_tags.sh
+++ exec/gen_tags.sh
@@ -61,7 +61,7 @@ if [ "$1" == "e" ]; then
 
 elif [ "$1" == "c" ]; then
     #ctags -R -e --sort=yes --c++-kinds=+px --fields=+faiKlSzm --extra=+q --exclude='*node_modules*' --exclude='bundle.js' --exclude=locales --exclude='*/build/*' --exclude=.git --exclude='*.min.js' --exclude='*.html' --exclude='-min.js' *
-    find -L $PWD -type f -not -iwholename '*TAGS' -not -size +16k ! \( -name "*.json" -o -name "*.gem" -o -name "*.html" -o -path "*.git*" -o -path "*node_modules" -o -name "bundle.js" -o -name "*.cache" -o -name "*.log" \) | ctags -f TAGS -e -L -
+    find -L $PWD -type f -not -iwholename '*TAGS' -not -size +16k ! \( -name "*.json" -o -name "*.gem" -o -name "*.html" -o -path "*.git*" -o -path "*node_modules*" -o -name "bundle.js" -o -name "*.cache" -o -name "*.log" \) | ctags -f TAGS -e -L -
 
     # lookupfile
     #echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/" > filenametags
