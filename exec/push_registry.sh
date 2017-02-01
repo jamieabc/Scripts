97d4a3b802053b7eb91ee5ee52fd33ee0410727e
Aaron Chang <aaron.chang@vpon.com>
2017-02-01 21:07:19 +0800

    modify scirpt name on server 29

--- exec/push_registry.sh
+++ exec/push_registry.sh
@@ -4,5 +4,5 @@ if [ $# == 0 ]; then
     echo "please input docker name to push"
     exit
 else
-    ssh -t buildmaster@192.168.101.29 "sh qa/push_to_registry_qa.sh dsp-web-$1"
+    ssh -t buildmaster@192.168.101.29 "sh qa/push_to_registry_from_29_to_qa.sh $1"
 fi
