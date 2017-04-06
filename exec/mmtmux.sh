55b0643fcae719016baab37fdf61745f7195f4ed
Aaron Chang <aaron.chang@vpon.com>
2017-04-06 10:01:56 +0800

    update tmux script

--- exec/mmtmux.sh
+++ exec/mmtmux.sh
@@ -7,21 +7,27 @@ tmux has-session -t ${SESSION_NAME}
 if [ $? != 0 ]
 then
     # start zeus in port 3000 @ vpon-dsp
-    tmux new-session -d -s ${SESSION_NAME} -n Zeus
-    tmux send-keys -t ${SESSION_NAME} 'cd ~/Documents/Project/vpon-dsp' C-m
-    tmux send-keys -t ${SESSION_NAME} 'zeus start' C-m
+    #tmux new-session -d -s ${SESSION_NAME} -n Zeus
+    #tmux send-keys -t ${SESSION_NAME} 'cd ~/Documents/Project/vpon-dsp' C-m
+    #tmux send-keys -t ${SESSION_NAME} 'zeus start' C-m
 
     # start rails server in zeus on port 3000 @ vpon-dsp
     #tmux new-window -t ${SESSION_NAME}:1 -n Rails 'cd ~/Documents/Project/vpon-dsp; sleep 5; zeus server'
-    tmux new-window -t ${SESSION_NAME}:1 -n Rails
-    tmux send-keys -t ${SESSION_NAME}:1 'cd ~/Documents/Project/vpon-dsp' C-m
-    tmux send-keys -t ${SESSION_NAME}:1 'spring stop; sleep 1; zeus server' C-m
+    #tmux new-window -t ${SESSION_NAME}:1 -n Rails
+    #tmux send-keys -t ${SESSION_NAME}:1 'cd ~/Documents/Project/vpon-dsp' C-m
+    #tmux send-keys -t ${SESSION_NAME}:1 'spring stop; sleep 1; zeus server' C-m
+
+
+    # start rails in port 3000 @ vpon-dsp
+    tmux new-session -d -s ${SESSION_NAME} -n rails
+    tmux send-keys -t ${SESSION_NAME} 'cd ~/Documents/Project/vpon-dsp' C-m
+    tmux send-keys -t ${SESSION_NAME} 'spring stop; rails s -p3000' C-m
 
     # run npm watch
     #tmux new-window -t ${SESSION_NAME}:2 -n npm-watch 'cd ~/Documents/Project/vpon-dsp/public/app/flux; npm run build:watch'
     tmux new-window -t ${SESSION_NAME}:2 -n npm-watch
     tmux send-keys -t ${SESSION_NAME}:2 'cd ~/Documents/Project/vpon-dsp/public/app/flux' C-m
-    tmux send-keys -t ${SESSION_NAME}:2 'npm run build:watch' C-m
+    tmux send-keys -t ${SESSION_NAME}:2 'npm run hot' C-m
 
     # tig
     #tmux new-window -t ${SESSION_NAME}:3 -n git
