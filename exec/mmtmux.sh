2068e6057897dbee4e2e9cfd9d4f8bdc24d4f79c
Aaron Chang <aaron.chang@vpon.com>
2017-06-27 17:11:14 +0800

    modify tmux for ssp

--- exec/mmtmux.sh
+++ exec/mmtmux.sh
@@ -6,39 +6,30 @@ tmux start-server
 tmux has-session -t ${SESSION_NAME}
 if [ $? != 0 ]
 then
-    # start zeus in port 3000 @ vpon-dsp
-    #tmux new-session -d -s ${SESSION_NAME} -n Zeus
-    #tmux send-keys -t ${SESSION_NAME} 'cd ~/Documents/Project/vpon-dsp' C-m
-    #tmux send-keys -t ${SESSION_NAME} 'zeus start' C-m
-
-    # start rails server in zeus on port 3000 @ vpon-dsp
-    #tmux new-window -t ${SESSION_NAME}:1 -n Rails 'cd ~/Documents/Project/vpon-dsp; sleep 5; zeus server'
-    #tmux new-window -t ${SESSION_NAME}:1 -n Rails
-    #tmux send-keys -t ${SESSION_NAME}:1 'cd ~/Documents/Project/vpon-dsp' C-m
-    #tmux send-keys -t ${SESSION_NAME}:1 'spring stop; sleep 1; zeus server' C-m
-
-
-    # start rails in port 3000 @ vpon-dsp
-    tmux new-session -d -s ${SESSION_NAME} -n rails
+    # dsp rails at port 3000
+    tmux new-session -d -s ${SESSION_NAME} -n dsp-rails
     tmux send-keys -t ${SESSION_NAME} 'cd ~/Documents/Project/vpon-dsp' C-m
     tmux send-keys -t ${SESSION_NAME} 'spring stop; rails s -p3000' C-m
 
-    # run npm watch
-    #tmux new-window -t ${SESSION_NAME}:2 -n npm-watch 'cd ~/Documents/Project/vpon-dsp/public/app/flux; npm run build:watch'
-    tmux new-window -t ${SESSION_NAME}:2 -n npm-watch
+    # dsp js bundle
+    tmux new-window -t ${SESSION_NAME}:2 -n dsp-js
     tmux send-keys -t ${SESSION_NAME}:2 'cd ~/Documents/Project/vpon-dsp/public/app/flux' C-m
     tmux send-keys -t ${SESSION_NAME}:2 'npm run hot' C-m
 
-    # tig
-    #tmux new-window -t ${SESSION_NAME}:3 -n git
-    #tmux send-keys -t ${SESSION_NAME}:3 'cd ~/Documents/Project/vpon-dsp' C-m
-    #tmux send-keys -t ${SESSION_NAME}:3 'tig' C-m
+    # ssp rails at port 4000
+    tmux new-window -t ${SESSION_NAME}:3 -n ssp-rails
+    tmux send-keys -t ${SESSION_NAME}:3 'cd ~/Documents/Project/ssp-web-api/' C-m
+    tmux send-keys -t ${SESSION_NAME}:3 'rails s -p4000'
 
-    # idp
+    # ssp js at port 4001
+    tmux new-window -t ${SESSION_NAME}:4 -n ssp-js
+    tmux send-keys -t ${SESSION_NAME}:4 'cd ~/Documents/Project/ssp-web-ui/' C-m
+    tmux send-keys -t ${SESSION_NAME}:4 'npm run start'
+
+    # id
     #tmux new-window -t ${SESSION_NAME}:3 -n idp
     #tmux send-keys -t ${SESSION_NAME}:3 'cd ~/Documents/Project/vpon-idp' C-m
     #tmux send-keys -t ${SESSION_NAME}:3 'spring stop; rails s -p3001' C-m
-
 
     tmux select-window -t ${SESSION_NAME}:2
 fi
