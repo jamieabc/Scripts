54603944ed0959ef24fae301372ac96f0b834cfa
Aaron Chang <aaron.chang@quadas.com>
2018-05-11 12:44:51 +0800

    update tmux script

--- exec/mmtmux.sh
+++ exec/mmtmux.sh
@@ -10,25 +10,25 @@ tmux start-server
 tmux has-session -t ${SESSION_NAME}
 if [ $? != 0 ]
 then
-    # dsp rails at port 3000
+    # account client side
     dir1="account-client"
     tmux new-session -d -s ${SESSION_NAME} -n $dir1
     tmux send-keys -t ${SESSION_NAME} "cd ${PROJECT_PATH}/${dir1}" C-m
     tmux send-keys -t ${SESSION_NAME} 'npm run watch:dev' C-m
 
-    # dsp js bundle
+    # webapp client side
     dir2="webapp-client"
     tmux new-window -t ${SESSION_NAME}:2 -n $dir2
     tmux send-keys -t ${SESSION_NAME}:2 "cd ${PROJECT_PATH}/${dir2}" C-m
     tmux send-keys -t ${SESSION_NAME}:2 'npm run watch:dev' C-m
 
-    # ssp rails at port 4000
+    # webapp server side
     dir3="webapp"
     tmux new-window -t ${SESSION_NAME}:3 -n ${dir3}
     tmux send-keys -t ${SESSION_NAME}:3 "cd ${PROJECT_PATH}/${dir3}" C-m
     tmux send-keys -t ${SESSION_NAME}:3 'go run main.go' C-m
 
-    # ssp js at port 4001
+    # accoutn server side
     dir4="account"
     tmux new-window -t ${SESSION_NAME}:4 -n $dir4
     tmux send-keys -t ${SESSION_NAME}:4 "cd ${PROJECT_PATH}/${dir4}" C-m
@@ -39,7 +39,7 @@ then
     #tmux send-keys -t ${SESSION_NAME}:3 'cd ~/Documents/Project/vpon-idp' C-m
     #tmux send-keys -t ${SESSION_NAME}:3 'spring stop; rails s -p3001' C-m
 
-    tmux select-window -t ${SESSION_NAME}:2
+    tmux select-window -t ${SESSION_NAME}:0
 fi
 
 tmux attach-session -t ${SESSION_NAME}
