6848c8694bd5f7e5b2dad74ceafb4bc76f65bbfd
Aaron Chang <aaron.chang@quadas.com>
2018-10-16 14:20:33 +0800

    hide terminal commands to wakatime

--- exec/bash-wakatime.sh
+++ exec/bash-wakatime.sh
@@ -14,7 +14,9 @@ pre_prompt_command() {
     entity=$(echo $(fc -ln -0) | cut -d ' ' -f1)
     [ -z "$entity" ] && return # $entity is empty or only whitespace
     git status &> /dev/null && local project="$(basename $(git rev-parse --show-toplevel))" || local project="Terminal"
-    (wakatime --write --plugin "bash-wakatime/$version" --entity-type app --project "$project" --entity "$entity" 2>&1 > /dev/null &)
+    # (wakatime --write --plugin "bash-wakatime/$version" --entity-type app --project "$project" --entity "$entity" 2>&1 > /dev/null &)
+    # use commands instead of aactual command
+    (wakatime --write --plugin "bash-wakatime/$version" --entity-type app --project "$project" --entity "commands" 2>&1 > /dev/null &)
 }
 
 PROMPT_COMMAND="pre_prompt_command; $PROMPT_COMMAND"
