f03557605f704a7fad584f61b21129de96a9000f
Aaron Chang <xesuschang@gmail.com>
2018-12-20 15:06:02 +0800

    update script to run

--- exec/switch_crypto.sh
+++ exec/switch_crypto.sh
@@ -32,6 +32,8 @@ switch_crypto() {
         ln -sf cli3.mac.conf cli3.conf > /dev/null
         ln -sf cli4.mac.conf cli4.conf > /dev/null
         ln -sf cli5.mac.conf cli5.conf > /dev/null
+        cd - 1 > /dev/null
+        cd ${HOME}/.config/wallet
         ln -sf wallet.mac.conf wallet.conf > /dev/null
         ln -sf wallet.mac.dat wallet.dat > /dev/null
         cd - 1 > /dev/null
@@ -48,6 +50,8 @@ switch_crypto() {
         ln -sf cli3.remote.conf cli3.conf > /dev/null
         ln -sf cli4.remote.conf cli4.conf > /dev/null
         ln -sf cli5.remote.conf cli5.conf > /dev/null
+        cd - 1 > /dev/null
+        cd ${HOME}/.config/wallet
         ln -sf wallet.remote.conf wallet.conf > /dev/null
         ln -sf wallet.remote.dat wallet.dat > /dev/null
         cd - 1 > /dev/null
