1d24fac85cb50c9135f04b5f16ffe5f1d8c130d4
Aaron Chang <xesuschang@gmail.com>
2018-12-26 11:37:38 +0800

    remove unused files

--- exec/push_docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ssh buildmaster@192.168.101.29 "sh qa/push_to_registry_qa.sh $1"
--- exec/push_registry.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $# == 0 ]; then
-    echo "please input docker name to push"
-    exit
-else
-    ssh -t buildmaster@192.168.101.29 "sh qa/push_to_registry_from_29_to_qa.sh $1"
-fi
