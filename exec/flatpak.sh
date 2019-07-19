d7776533602a333404269be520cb14eed97066a9
Aaron Chang <xesuschang@gmail.com>
2019-07-19 14:40:27 +0800

    add flatpak install script

---
+++ exec/flatpak.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ "$#" -eq "0" ]; then
+    # install libraries
+    sudo apt install -y flatpak flatpak-builder
+    flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    flatpak install -y flathub org.freedesktop.Platform//1.6 org.freedesktop.Sdk//1.6 --user
+
+    # install flatpak build
+    flatpak install -y --user bitmarkd.flatpak
+elif [ "$1" = "u" ] || [ "$1" = "uninstall" ]; then
+    flatpak uninstall com.bitmark.bitmarkd
+fi
