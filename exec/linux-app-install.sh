da4fbff9fed795f56cd000aaac824e01485e8af5
Aaron Chang <xesuschang@gmail.com>
2019-08-28 10:40:46 +0800

    add openvpn to linux install script

--- exec/linux-app-install.sh
+++ exec/linux-app-install.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install rxvt-unicode xsel argon2
+sudo apt install rxvt-unicode xsel argon2 openvpn
 
 # haskell
 # reference from https://docs.haskellstack.org/en/stable/README/
