e2dd19e9baba2dce6e5dda47f22613637a8818d5
Aaron Chang <xesuschang@gmail.com>
2019-08-28 10:37:16 +0800

    add haskell to linux install script

--- exec/linux-app-install.sh
+++ exec/linux-app-install.sh
@@ -3,3 +3,11 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install rxvt-unicode xsel argon2
+
+# haskell
+# reference from https://docs.haskellstack.org/en/stable/README/
+curl https://get-ghcup.haskell.org -sSf | sh
+curl -sSL https://get.haskellstack.org/ | sh
+stack update
+stack upgrade
+stack install hasktags happy stylish-haskell present hlint hoogle hindent
