ab4c43479730b12970725146e5cef77fa7304da3
Aaron Chang <xesuschang@gmail.com>
2019-09-02 11:26:25 +0800

    update script

--- exec/nodes.sh
+++ exec/nodes.sh
@@ -2,20 +2,22 @@
 require "json"
 
 hosts = {
-    production: "nodes.live.bitmark.com",
-    staging: "nodes.test.bitmark.com"
+  production: "nodes.live.bitmark.com",
+  staging: "nodes.test.bitmark.com"
 }
 
 cmd = "host -t txt"
 
-def parse_result (line)
+def parse_result(line)
   ip4, ip6, public_key = line.match(/a=(.*);\[(.*)\].*p=(.*)"/).captures
   puts "#{ip4}\t\t#{ip6}\t\t#{public_key}\n"
 end
 
 hosts.each do |category, host|
-  puts "#{category}"
+  puts category.to_s
   `#{cmd} #{host}`.split("\n").each do |line|
+    next if line.start_with?(';')
+
     parse_result line
   end
 end
