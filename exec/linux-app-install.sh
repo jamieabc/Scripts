37db050eceec828eb123cd3a323e410b716401e8
Aaron Chang <xesuschang@gmail.com>
2019-11-25 18:45:03 +0800

    install zsh

--- exec/linux-app-install.sh
+++ exec/linux-app-install.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install rxvt-unicode xsel argon2 openvpn xdotool zenity ibus libxcb-xtest0 bash-completion resolvconf
+sudo apt install rxvt-unicode xsel argon2 openvpn xdotool zenity ibus libxcb-xtest0 bash-completion resolvconf zsh
 
 # haskell
 # reference from https://docs.haskellstack.org/en/stable/README/
