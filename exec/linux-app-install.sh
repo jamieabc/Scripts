7a979b817072a9335d958088ebd80e0c9f773deb
Aaron Chang <xesuschang@gmail.com>
2019-11-30 12:05:36 +0800

    install nodejs

--- exec/linux-app-install.sh
+++ exec/linux-app-install.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install rxvt-unicode xsel argon2 openvpn xdotool zenity ibus libxcb-xtest0 bash-completion resolvconf zsh
+sudo apt install rxvt-unicode xsel argon2 openvpn xdotool zenity ibus libxcb-xtest0 bash-completion resolvconf zsh nodejs
 
 # haskell
 # reference from https://docs.haskellstack.org/en/stable/README/
