c38c084765a4663e4c53ed59bd6672f2627377b0
Aaron Chang <xesuschang@gmail.com>
2019-12-12 10:51:36 +0800

    update zsh install

--- exec/linux-app-install.sh
+++ exec/linux-app-install.sh
@@ -2,7 +2,15 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install rxvt-unicode xsel argon2 openvpn xdotool zenity ibus libxcb-xtest0 bash-completion resolvconf zsh nodejs
+sudo apt install rxvt-unicode xsel argon2 openvpn xdotool zenity ibus libxcb-xtest0 bash-completion resolvconf zsh nodejs fd-find fzf ripgrep
+
+# fasd
+sudo add-apt-repository ppa:aacebedo/fasd
+sudo apt-get update
+sudo apt-get install fasd
+
+# zplugin
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
 
 # haskell
 # reference from https://docs.haskellstack.org/en/stable/README/
