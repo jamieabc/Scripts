232ec276b034bf250bd2c1468bc9fb52cda38796
Aaron Chang <xesuschang@gmail.com>
2020-01-03 14:43:19 +0800

    modify install script

--- exec/linux-app-install.sh
+++ exec/linux-app-install.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install rxvt-unicode xsel argon2 openvpn xdotool zenity ibus libxcb-xtest0 bash-completion resolvconf zsh nodejs fd-find fzf ripgrep
+sudo apt install rxvt-unicode xsel argon2 openvpn xdotool zenity ibus libxcb-xtest0 bash-completion resolvconf zsh nodejs fd-find fzf ripgrep stunnel
 
 # fasd
 sudo add-apt-repository ppa:aacebedo/fasd
