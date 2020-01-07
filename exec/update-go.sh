b7353d8e57cbf47b6fb7518ea50752c482fb13b5
Aaron Chang <xesuschang@gmail.com>
2020-01-07 15:02:37 +0800

    update script

--- exec/update-go.sh
+++ exec/update-go.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
 go get -u github.com/stamblerre/gocode
-go get -u golang.org/x/tools
-go get -u golang.org/x/tools/cmd/guru
-go get -u golang.org/x/tools/cmd/goimports
-go get -u golang.org/x/tools/cmd/godoc
+go get -u golang.org/x/tools/cmd/...
+go get -u github.com/google/pprof
 go install golang.org/x/tools/cmd/gorename
-# go get -u golang.org/x/tools/cmd/...
 go get -u github.com/josharian/impl
 go get -u github.com/fatih/gomodifytags
 go get -u github.com/davidrjenni/reftools/cmd/fillstruct
@@ -14,10 +11,9 @@ go get -u github.com/uudashr/gopkgs/cmd/gopkgs
 go get -u golang.org/x/lint/golint
 go get -u github.com/rogpeppe/godef
 go get -u github.com/dougm/goflymake
-curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(go env GOPATH)/bin v1.17.1
 go get -u github.com/zmb3/gogetdoc
 go get -u github.com/cweill/gotests
-go get golang.org/x/tools/gopls@latest
+GO111MODULE=on go get golang.org/x/tools/gopls@latest
 go get -u github.com/go-delve/delve/cmd/dlv
 go get -u github.com/TrueFurby/go-callvis
 go get -u github.com/golang/mock/gomock
