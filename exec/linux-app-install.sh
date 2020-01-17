b176af38e28e6e55f6a4c86656c780632f02749d
Aaron Chang <xesuschang@gmail.com>
2020-01-17 18:58:37 +0800

    update linux install script

--- exec/linux-app-install.sh
+++ exec/linux-app-install.sh
@@ -1,8 +1,34 @@
 #!/usr/bin/env bash
 
+cd ~/exec/
+
 sudo apt update
 sudo apt upgrade -y
-sudo apt install rxvt-unicode xsel argon2 openvpn xdotool zenity ibus libxcb-xtest0 bash-completion resolvconf zsh nodejs fd-find fzf ripgrep stunnel
+sudo apt -y install curl rxvt-unicode xsel argon2 openvpn xdotool zenity ibus libxcb-xtest0 bash-completion resolvconf zsh nodejs stunnel4 zsh cargo fonts-firacode build-essential wmctrl
+
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# ripgrep
+git clone https://github.com/BurntSushi/ripgrep
+cd ripgrep
+cargo build --release
+[ -d $HOME/exec ] && mv ./target/release/rg $HOME/exec/
+cd ..
+rm -rf ripgrep
+
+# fd-find
+wget https://github.com/sharkdp/fd/releases/download/v7.4.0/fd-musl_7.4.0_amd64.deb
+sudo dpkg -i fd-musl_7.4.0_amd64.deb
+rm fd-musl_7.4.0_amd64.deb
+
+# kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod u+x ./kubectl
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 
 # fasd
 sudo add-apt-repository ppa:aacebedo/fasd
@@ -23,3 +49,6 @@ stack install hasktags happy stylish-haskell present hlint hoogle hindent
 # markdown preview
 npm -g install instant-markdown-d
 pip3 install --user smdv
+
+# zsh-notify
+git clone git@github.com:marzocchi/zsh-notify.git ~/.zsh/custom/plugins/notify
