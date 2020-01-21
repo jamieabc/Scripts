d0f7036762d365eba60e614ce252a37484767229
Aaron Chang <xesuschang@gmail.com>
2020-01-21 19:45:52 +0800

    update linux script

--- exec/linux-app-install.sh
+++ exec/linux-app-install.sh
@@ -63,6 +63,8 @@ sudo apt install golang-go
 go get github.com/charmbracelet/glow
 
 # rvm
-sudo apt-add-repository -y ppa:rael-gc/rvm
-sudo apt-get update
-sudo apt-get install rvm
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash
+
+# gems
+# gem install ghi
