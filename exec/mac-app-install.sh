c5880badb5311f33206bbb02a65bbedfab307ebf
Aaron Chang <xesuschang@gmail.com>
2020-03-12 15:38:57 +0800

    update script

--- exec/mac-app-install.sh
+++ exec/mac-app-install.sh
@@ -104,7 +104,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 nvm install node && nvm alias default node
 
 # install node packages
-npm install -g js-beautify shelljs jsonlint npm-check-updates jshint jsxhint tern tern-highlight abel babel-eslint bower gulp gulp-cli grunt grunt-cli redmine-cli-modified eslint eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y eslint-plugin-jsx eslint-config-airbnb eslint-plugin-react csslint osprey yarn eslint-plugin-flowtype coffee-script esformatter esformatter-jsx esformatter-remove-trailing-commas i18next-parser prettier @vue/cli @angular/cli eslint-plugin-angular eslint-config-angular tslint-eslint-rules sass-lint wscat typescript forever nodemon http-server react-devtools appium appium-doctor authorize-ios vue-language-server javascript-typescript-langserver truffle ganache-cli gitbook-cli instant-markdown-d skygragon/leetcode-cli
+npm install -g js-beautify shelljs jsonlint npm-check-updates jshint jsxhint tern tern-highlight abel eslint eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y eslint-plugin-jsx eslint-config-airbnb eslint-plugin-react csslint osprey yarn eslint-plugin-flowtype coffeescript esformatter esformatter-jsx esformatter-remove-trailing-commas prettier @vue/cli @angular/cli eslint-plugin-angular eslint-config-angular tslint-eslint-rules sass-lint wscat typescript forever nodemon http-server react-devtools vue-language-server javascript-typescript-langserver truffle ganache-cli gitbook-cli instant-markdown-d skygragon/leetcode-cli
 
 # clone tmux plugin manager (tpm)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
