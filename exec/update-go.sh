a3bb58a39aa210186c1399ddd48a2e42245e4826
Aaron Chang <xesuschang@gmail.com>
2020-08-03 11:12:54 +0800

    update go udpate script

--- exec/update-go.sh
+++ exec/update-go.sh
@@ -18,9 +18,9 @@ go get -u github.com/go-delve/delve/cmd/dlv
 go get -u github.com/TrueFurby/go-callvis
 go get -u github.com/golang/mock/gomock
 go get -u github.com/golang/mock/mockgen
-go get -u github.com/vektra/mockery/cmd/mockery
+go get -u github.com/vektra/mockery/.../
 go get -u github.com/lukehoban/go-outline
-go get -u honnef.co/go/tools
+go get -u honnef.co/go/tools/cmd/...
 go get -u github.com/godoctor/godoctor
 go get github.com/securego/gosec/cmd/gosec
 go get -u github.com/charmbracelet/glow # render markdown in CLI
